9ab729dfbb90d4bb2fab1842e1ad3abd17e01f7a
lilinyu <damonyu97@gmail.com>
2025-11-04 16:46:34 +0000

    google cloud storage for input and output

--- event_extraction/extract_broadsides_events.py
+++ event_extraction/extract_broadsides_events.py
@@ -1,9 +1,16 @@
-import pandas as pd
+import json
 from omni.extractor import Extractor
+from utils import get_google_cloud_storage
+
 if __name__ == '__main__':
     # load dataframe
-    df = pd.read_json("../generated_files/broadsides_subs_kg_hq_df", orient="index")
+    cloud_storage = get_google_cloud_storage()
+    input_filename = "ee/sources/broadsides_subs_kg_hq_df"
+    df = cloud_storage.read_pandas_json(input_filename, orient="index")
+    print(f"{len(df)} broadsides loaded!")
     extractor = Extractor()
     texts = df["description"].values.tolist()
-    results = extractor.extract_events(texts[:10])
-    print(results)
+    results = extractor.extract_events(texts)
+    output_filename = "ee/results/broadsides_ee.json"
+    print(f"Saving results to {output_filename}")
+    cloud_storage.write_str(json.dumps(results), output_filename)
--- event_extraction/omni/extractor.py
+++ event_extraction/omni/extractor.py
@@ -1,3 +1,5 @@
+import time
+
 import torch
 from OmniEvent.infer import get_pretrained
 from OmniEvent.infer_module.seq2seq import do_event_detection, prepare_for_eae_from_pred, do_event_argument_extraction, \
@@ -12,13 +14,17 @@ class Extractor:
                 self.device = torch.device("cuda")
         else:
             self.device = torch.device(device)
+        print(f"Using device: {self.device}")
         self.ed_model, self.ed_tokenizer = get_pretrained("s2s-mt5-ed", self.device)
         self.eae_model, self.eae_tokenizer = get_pretrained("s2s-mt5-eae", self.device)
 
     def extract_events(self, texts, schema="ace"):
         schemas = len(texts) * [f"<{schema}>"]
-
+        print(f"Extracting events for {len(texts)} texts...")
+        print(f"Running event detection.....")
+        start_time = time.time()
         events = do_event_detection(self.ed_model, self.ed_tokenizer, texts, schemas, self.device)
+        print("Running event argument extraction...")
         instances = prepare_for_eae_from_pred(texts, events, schemas)
         if len(instances[0]["triggers"]) == 0:
             results = [{
@@ -27,6 +33,8 @@ class Extractor:
             }]
             return results
         arguments = do_event_argument_extraction(self.eae_model, self.eae_tokenizer, instances, self.device)
+        print(f"Getting event extraction results...")
         results = get_eae_result(instances, arguments)
+        print(f"Event extraction is completed in {time.time() - start_time:.2f} seconds.")
         return results
 
